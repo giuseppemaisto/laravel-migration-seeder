63799e0fdbff5704c7caef017ae6c5e901f97418
giuseppemaisto <gmaisto98@gmail.com>
2023-02-21 16:28:01 +0100

    completo tutto, problema con stampa

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
-    //
+    public function index(){
+        $trains = Train::all();
+        return view('home', compact('trains'));
+    }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+    foreach($trains as $train)
+
+    <p>azienda: {{$train['Azienda']}}</p>
+    </div>
+</div>
+
+</body>
+
+</html>
